f79b74e92b63ca3ff6a7093368978022dc8f09ba
Artur <>
2024-04-16 22:29:43 +0300

    main

---
+++ kb-backend-week-2-startkit-main/controllers/index.js
@@ -0,0 +1,11 @@
+const gameRouteController = require("./game");
+const mainRouteController = require("./main");
+const voteRouteController = require("./vote");
+const defaultRouteController = require("./default");
+
+module.exports = {
+  gameRouteController,
+  mainRouteController,
+  voteRouteController,
+  defaultRouteController,
+};
